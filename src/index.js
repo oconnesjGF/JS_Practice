dd53e22de996e44ce01f30f9a68548296b345245
oconnesjGF <104014724+oconnesjGF@users.noreply.github.com>
2022-04-26 09:20:48 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/*
+// ES6 
+const myAge = 29;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(`Early years is: ${earlyYears}`);
+console.log(`Later years is: ${laterYears}`);
+let myAgeInDogYears = earlyYears + laterYears;
+console.log(`My age in Dog Years is: ${myAgeInDogYears}`);
+let myName = "Sam O'Connell".toLowerCase();
+//myName = myName.toLowerCase();
+//console.log(myName);
+
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
+*/
+
+// ES5
+var myAge = 29;
+var earlyYears = 2;
+earlyYears *= 10.5;
+var laterYears = myAge - 2;
+laterYears *= 4;
+console.log(`Early years is: ${earlyYears}`);
+console.log(`Later years is: ${laterYears}`);
+var myAgeInDogYears = earlyYears + laterYears;
+console.log(`My age in Dog Years is: ${myAgeInDogYears}`);
+var myName = "Sam O'Connell".toLowerCase();
+//myName = myName.toLowerCase();
+//console.log(myName);
+
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
